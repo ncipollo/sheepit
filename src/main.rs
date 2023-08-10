48774c67e5e39ad60a9dbbe5d5b770fc5c97ef0e
Nick Cipollo <njc1115@gmail.com>
2023-08-10 17:08:19 -0400

    Add deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
+    let dir = tempfile::tempdir().
+        expect("unable to create tmp dir");
+    let path = dir.path();
+    println!("{:#?}", path);
     println!("Hello, world!");
 }
