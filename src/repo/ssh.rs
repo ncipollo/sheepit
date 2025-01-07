5443efe654dbe435a690a5bb53a746e1a5f9039e
Nick Cipollo <njc1115@gmail.com>
2025-01-06 22:19:39 -0500

    Provide mechanism to override ssh key path via env

--- src/repo/ssh.rs
+++ src/repo/ssh.rs
@@ -1,11 +1,16 @@
-use std::fs;
-use std::path::{Path, PathBuf};
-use git2::{Cred, Error};
 use crate::SheepError;
+use git2::{Cred, Error};
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 /// Returns the standard path to the user's ssh key.
 fn ssh_key_path() -> String {
-    find_best_ssh_key().expect("failed to find ssh key")
+    path_from_env().unwrap_or_else(|| find_best_ssh_key().expect("failed to find ssh key"))
+}
+
+fn path_from_env() -> Option<String> {
+    let path = env::var("SHEEPIT_SSH_KEY_PATH");
+    path.ok().map(|p| shellexpand::tilde(&p).to_string())
 }
 
 fn find_best_ssh_key() -> Result<String, SheepError> {
@@ -20,38 +25,29 @@ fn find_best_ssh_key() -> Result<String, SheepError> {
 }
 
 fn ssh_file_names(ssh_dir_path: PathBuf) -> Result<Vec<String>, SheepError> {
-    let names = fs::read_dir(ssh_dir_path)
-        ?.map(|res| res.map(|e| e.file_name()))
-        .map({
-            |res| res.map_or("".to_string(),
-                             |name| name.to_string_lossy().to_string())
-        })
+    let names = fs::read_dir(ssh_dir_path)?
+        .map(|res| res.map(|e| e.file_name()))
+        .map({ |res| res.map_or("".to_string(), |name| name.to_string_lossy().to_string()) })
         .collect::<Vec<_>>();
     Ok(names)
 }
 
 fn find_best_key_name(file_names: Vec<String>) -> Result<String, SheepError> {
-    file_names.iter()
-        .find(|name| {
-            !name.ends_with(".pub") && name.starts_with("id_")
-        })
+    file_names
+        .iter()
+        .find(|name| !name.ends_with(".pub") && name.starts_with("id_"))
         .map(|name| name.to_string())
         .ok_or(SheepError::new("failed to find ssh key"))
 }
 
 pub fn add_credentials_to_callbacks(remote_callbacks: &mut git2::RemoteCallbacks) {
-    remote_callbacks.credentials(|_, username_from_url, _| {
-        create_ssh_key(username_from_url.unwrap())
-    });
+    remote_callbacks
+        .credentials(|_, username_from_url, _| create_ssh_key(username_from_url.unwrap()));
 }
 
 pub fn create_ssh_key(username_from_url: &str) -> Result<Cred, Error> {
-    Cred::ssh_key(
-        username_from_url,
-        None,
-        Path::new(&ssh_key_path()),
-        None,
-    )
+    let ssh_key_path = ssh_key_path();
+    Cred::ssh_key(username_from_url, None, Path::new(&ssh_key_path), None)
 }
 
 #[cfg(test)]
@@ -66,17 +62,8 @@ mod test {
 
     #[test]
     fn find_best_key_name_typical_ssh() {
-        let names = vec![
-            "config",
-            "id_rsa",
-            "id_rsa.pub",
-            "known_hosts",
-        ];
-        let result = find_best_key_name(
-            names.iter()
-                .map(|n| n.to_string())
-                .collect()
-        );
+        let names = vec!["config", "id_rsa", "id_rsa.pub", "known_hosts"];
+        let result = find_best_key_name(names.iter().map(|n| n.to_string()).collect());
         let name = result.expect("failed to find file name");
         assert_eq!("id_rsa", name)
     }
